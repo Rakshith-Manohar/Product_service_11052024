165534a221a951197f297157c8b04cbabc62316c
Rakshith <Rakshith@example.com>
2024-05-17 21:25:51 +0530

    Added JPA dependency

--- src/main/java/com/example/product_service_11052024/models/Category.java
+++ src/main/java/com/example/product_service_11052024/models/Category.java
@@ -1,10 +1,18 @@
 package com.example.product_service_11052024.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class Category {
+@Entity
+public class Category extends BaseModel{
     private String title;
+
+    @OneToMany(mappedBy = "category")
+    List<Product> products;
 }
--- src/main/java/com/example/product_service_11052024/models/Product.java
+++ src/main/java/com/example/product_service_11052024/models/Product.java
@@ -1,16 +1,21 @@
 package com.example.product_service_11052024.models;
 
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.engine.internal.Cascade;
 
 @Getter
 @Setter
-public class Product {
-    private int id;
+@Entity
+public class Product extends BaseModel {
     private String title;
     private String description;
     private double price;
     private String imageUrl;
+    @ManyToOne(cascade = {CascadeType.PERSIST})
     private Category category;
 }
